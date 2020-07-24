9666d2c70750971c9174e8f89a99dd852fe12c0b
Abhinav <abhinavrgupta98@gmail.com>
2020-07-24 23:40:40 +0530

    v2 setup show route and add campground model

--- app.js
+++ app.js
@@ -1,57 +1,89 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var campgrounds = [
-  {
-    name: "agnes",
-    image:
-      "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "ulrich",
-    image:
-      "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "johnas",
-    image:
-      "https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    name: "martha",
-    image:
-      "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-];
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "khajjiar",
+//     image: "https://cdn.s3waas.gov.in/s3577bcc914f9e55d5e4e4f82f9f00e7d4/uploads/bfi_thumb/2018040364-olw8nged76z1v26p21o05b496dbiz7mbqck8frguq2.jpg",
+//     description: "Snowy, hilly, with a small lake.Paragliding available!",
+//   }, function (err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("NEW CAMPGROUND CREATED: ");
+//       console.log(campground);
+//     }
+//   });
+
+
 
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
+// INDEX ROUTE - show all campmgrounds
 app.get("/campgrounds", function (req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  //GET ALL campgrounds from DB
+  Campground.find({}, function (err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allCampgrounds });
+    }
+  })
 });
 
+// CREATE ROUTE - add new camp to the database
 app.post("/campgrounds", function (req, res) {
   //GET DATA FROM THE FORM AND ADD TO CAMPGROUNDS 
   // REDIRECT BACK TO CAMPGROUNDS PAGE
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
+  var description = req.body.description;
+  var newCampground = { name: name, image: image, description: description };
+  //CREATE A NEW CAMPGROUND AND SAVE TO DATABASE
+  Campground.create(newCampground, function (err, newlyCreated) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
 
-  res.redirect("/campgrounds");
-})
-
+// NEW ROUTE- show form to create new campground
 app.get("/campgrounds/new", function (req, res) {
   res.render("new");
 })
 
-
+//SHOW ROUTE - shows more info about one campground
+app.get("/campgrounds/:id", function (req, res) {
+  //find campground with provided id
+  Campground.findById(req.params.id, function (err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render("show", { campground: foundCampground });
+    }
+  });
+});
 
 var port = process.env.PORT || 3001;
 app.listen(port, process.env.IP, function () {
