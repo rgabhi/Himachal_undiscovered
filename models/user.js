0932ac9410bc32e909e8c87cf8552c3c6178764a
Abhinav <abhinavrgupta98@gmail.com>
2020-08-06 10:11:29 +0530

    add user profile and signup updates

--- models/user.js
+++ models/user.js
@@ -3,6 +3,12 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avatar: {
+        type: String, default: "https://images.unsplash.com/photo-1595744197503-47a9f334692f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    firstName: String,
+    lastName: String,
+    email: String,
     isAdmin: { type: Boolean, default: false },
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 
 
 //ROOT ROUTE
@@ -18,7 +19,13 @@ router.get("/register", function (req, res) {
 
 //handle sign up logic
 router.post("/register", function (req, res) {
-    var newUser = new User({ username: req.body.username });
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar,
+    });
     if (req.body.adminCode === "valar_morghulis") {
         newUser.isAdmin = true;
     }
@@ -52,6 +59,24 @@ router.get("/logout", function (req, res) {
     req.logout();
     req.flash("success", "You have been logged out!");
     res.redirect("/campgrounds");
+});
+
+// USERS ROUTE
+router.get("/users/:id", function (req, res) {
+    User.findById(req.params.id, function (err, foundUser) {
+        if (err) {
+            req.flash("error", "Something went wrong.");
+            return res.redirect("/");
+        }
+        Campground.find().where('author.id').equals(foundUser._id).exec(function (err, campgrounds) {
+            if (err) {
+                req.flash("error", "Something went wrong.");
+                return res.redirect("/");
+            }
+            res.render("users/show", { user: foundUser, campgrounds: campgrounds });
+        });
+
+    });
 });
 
 module.exports = router;
